265e9d881e3336733e01679faa03f70d05ec7dd6
Leonardo Teixeira <leonardo.teixeira.dev@gmail.com>
2022-06-08 13:48:33 -0300

    Create class Piece and Board

--- src/br/com/consoletech/application/Program.java
+++ src/br/com/consoletech/application/Program.java
@@ -1,7 +1,12 @@
 package br.com.consoletech.application;
 
+import br.com.consoletech.boardgame.Board;
+
 public class Program {
 	public static void main(String[] args) {
-		
+		System.out.println("Welcome to Super-Chess ");
+		Board board = new Board( 8, 8 );
+		System.out.println("lnhas: "+board.getRows());
+		System.out.println("Colunas: "+board.getColumns());
 	}
 }
